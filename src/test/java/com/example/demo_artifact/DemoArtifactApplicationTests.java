a165ab8c4348193b8141abc2beb3358ef42f055a
froll123 <froll_123@mail.ru>
2024-07-17 09:35:26 +0300

    Task 3

---
+++ src/test/java/com/example/demo_artifact/DemoArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
